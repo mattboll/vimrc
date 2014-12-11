19cab276b6e9d082ea847421b9be9c11c8e8213f
Matthieu Bollot <m.bollot@code-troopers.com>
2014-12-11 17:54:39 +0100

    Added plugin eunuch

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ cp .vimrc .vimrc.bepo ~
 mkdir -p .vim/autoload
 curl -Sso .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim || \
 fetch -o .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim || \
-echo couldn\'t get pathogen && exit 404
+echo couldn\'t get pathogen
 cp -r .vim ~
 cd ~/.vim/bundle
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
