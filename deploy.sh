dc6af0a58f1b26020909938c7ed140d682c3369d
Matthieu Bollot <m.bollot@code-troopers.com>
2014-12-11 17:39:34 +0100

    Added fugitive plugin

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,9 @@
 git submodule init && git submodule update
 cp .vimrc .vimrc.bepo ~
 mkdir -p .vim/autoload
-curl -Sso .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
-fetch -o .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+curl -Sso .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim || \
+fetch -o .vim/autoload/pathogen.vim https://raw.githubusercontent.com/tpope/vim-pathogen/master/autoload/pathogen.vim || \
+echo couldn\'t get pathogen && exit 404
 cp -r .vim ~
+cd ~/.vim/bundle
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
